dcec9d942146aec3717cbd8990208bce5a5add84
tigerony <williamlei95906@gmail.com>
2022-11-08 06:10:58 +0900

    update layout name

--- src/App.js
+++ src/App.js
@@ -3,25 +3,23 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { initialCrossword, WalletContext } from "./contexts/accounts";
 
 import { Routes, Route } from "react-router-dom";
-import Loadable from './utils/loadable'
+import Loadable from "./utils/loadable";
 
 import { MUISnackbar } from "./utils/Sanckbar";
 import { Buffer } from "buffer";
-import 'react-toastify/dist/ReactToastify.css';
-import './App.css'
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
-const Layout = Loadable(lazy(() => import("./component/Layouts")));
-const NFTList = Loadable(lazy(() => import("./pages/List")))
-const RarityRank = Loadable(lazy(() => import("./pages/RarityRank")))
-const Mint = Loadable(lazy(() => import("./pages/Mint")))
-
+const Layout = Loadable(lazy(() => import("./component/AAA")));
+const NFTList = Loadable(lazy(() => import("./pages/List")));
+const RarityRank = Loadable(lazy(() => import("./pages/RarityRank")));
+const Mint = Loadable(lazy(() => import("./pages/Mint")));
 
 // eslint-disable-next-line no-undef
 globalThis.Buffer = Buffer;
 
 export default function App() {
   const [wallet, setWallet] = useState(undefined);
-
 
   const init = useCallback(async () => {
     const wallet_ = await initialCrossword();
@@ -32,13 +30,11 @@ export default function App() {
     init();
   }, [init]);
 
-
-
   return (
     <WalletContext.Provider value={wallet}>
       {/* <Suspense fallback={<Spinner />}> */}
       <Router>
-        <Routes >
+        <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<NFTList />} />
             <Route path="/list" element={<NFTList />} />
